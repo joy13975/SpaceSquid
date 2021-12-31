23a6d5f8e88892858c02cc93ed02bfd161864b9d
Joy Yeh <joyyeh.tw@gmail.com>
2021-12-31 23:00:15 +0900

    Sidebar update and reward source change

--- app/space_squid.py
+++ app/space_squid.py
@@ -7,56 +7,97 @@ import pandas as pd
 import streamlit as st
 from streamlit_autorefresh import st_autorefresh
 
-# st.set_page_config(layout='wide')
 from util import *
 
-page_refresh_interval = 30  # seconds
+st.set_page_config(page_title='SpaceSquid')
+
+page_refresh_interval = 60  # seconds
 refresh_count = st_autorefresh(interval=page_refresh_interval * 1000)
 page_last_refresh_time = datetime.now()
 
-'''# SpaceSquid
----'''
-
-coin_prices = get_coin_price('ethereum', 'gala', 'town-star')
-f'''### Coin prices
-| ETH | GALA | TOWN |
-|-----|------|------|
-| ${coin_prices["ethereum"]:.1f} | ${coin_prices["gala"]:.3f} | ${coin_prices["town-star"]:.3f} |
----
-'''
-
-'''### Status'''
-status_text = st.empty()
-status_text.write('Idle')
-
-
 rewards = fetch_rewards()
-reward_item_names = rewards.NFT.str.lower().str.strip().values
+reward_item_names = lowertrim(rewards.name).values
 def filter_asset_by_name(a):
     # Filter by item name
     if 'name' in a and\
         isinstance(a['name'], str) and \
-        any(a['name'].lower().strip() in n for n in reward_item_names):
+        any(lowertrim(a['name']) in n for n in reward_item_names):
         return True
     return False
 
+coin_prices = fetch_coin_prices('ethereum', 'gala', 'town-star')
+
+st.markdown(
+    f'''
+        <style>
+            section[data-testid="stSidebar"] {{
+                width: 40%;
+            }}
+            section[data-testid="stSidebar"] > div {{
+                width: 100%;
+            }}
+            table {{
+                width: 100%;
+            }}
+            button[kind="primary"] {{
+                width: 100%;
+            }}
+        </style>
+    ''',
+    unsafe_allow_html=True
+)
+
+with st.sidebar:
+    status_text = st.empty()
+    item_count_text = st.empty()
+    def update_status(s):
+        status_text.write('## Status: ' + s)
+
+    for line in [
+        '## Coin prices \n',
+        f'''
+        | ETH | GALA | TOWN |
+        | --- | --- | --- |
+        | ${coin_prices["ethereum"]:.1f} | ${coin_prices["gala"]:.3f} | ${coin_prices["town-star"]:.3f} |
+        '''
+        ]:
+        st.markdown(line)
+
+    st.write('\n')
+    update_status('Idle')
+
+    cols = st.columns([0.6, 0.4])
+    with cols[0]:
+        update_token_id_btn = st.button('Update Token IDs')
+    with cols[1]:
+        update_prices_btn = st.button('Update Prices')
+    cols = st.columns([0.6, 0.4])
+    with cols[0]:
+        price_sort_map = {
+            'Change': 'OS Change',
+            'DTC': 'DTC',
+            'Arb': 'Arb',
+            'Price': 'OS USD',
+            'GS Qty': 'GS Qty'
+        }
+        sort_option = st.selectbox(label='Sort By', options=price_sort_map.keys())
+    with cols[1]:
+        sort_order = st.selectbox(label='Order', options=['ASC', 'DESC'])
+
+
 def update_token_ids():
-    assets = list(fetch_items(status_text, attribute_filter=filter_asset_by_name))
-    f'{len(assets)} items fetched'
+    assets = list(fetch_items(update_status, attribute_filter=filter_asset_by_name))
     return [a['token_id'] for a in assets], assets
 
 
 token_id_csv = 'data/town_star_token_ids.csv'
 token_ids = pd.read_csv(token_id_csv) if os.path.exists(token_id_csv) else None
 assets = None
-if token_ids is None or st.button('Update Token IDs'):
+if token_ids is None or update_token_id_btn:
     token_ids, assets = update_token_ids()
     token_ids = pd.DataFrame(token_ids, columns=['token_id'])
     token_ids.to_csv(token_id_csv, index=False)
-    status_text.write(f'Token ID list updated (n={len(token_ids)}); assets fetched')
-
-with st.expander('Token IDs'):
-    token_ids
+    update_status(f'Token ID list updated (n={len(token_ids)}); assets fetched')
 
 def filter_asset_by_token_id(a):
     return 'token_id' in a and a['token_id'] in token_ids
@@ -66,41 +107,41 @@ prices = pd.read_csv(prices_csv) if os.path.exists(prices_csv) else None
 last_price_refresh = datetime.now() - timedelta(seconds=page_refresh_interval*2) \
     if prices is None else datetime.fromisoformat(prices['LastUpdate'].iloc[0])
 expired = (datetime.now() - last_price_refresh).total_seconds() > page_refresh_interval
-if prices is None or st.button('Update Prices') or expired:
-    status_text.write('Updating prices...')
-    assets = fetch_items(token_ids=token_ids.token_id)
+if prices is None or update_prices_btn or expired:
+    if update_prices_btn:
+        print('Update price button pressed')
+    if expired:
+        print('Prices expired')
+    update_status('Updating prices...')
+    assets = list(fetch_items(token_ids=token_ids.token_id))
     prices = parse_prices(assets)
 
     def get_reward(name):
-        name_match = (rewards.NFT == name) | (rewards.NFT.str.lower().str.startswith(name.lower()))
+        name_match = (rewards.name == name)
         if not any(name_match):
-            print(f'No reward info found for {name}')
+            # Sometimes names don't match up exactly between data sources
+            name_match = (lowertrim(rewards.name).str.startswith(lowertrim(name)))
+        if not any(name_match):
+            print(f'Warning: Reward info unavailable for {name}')
             return float('nan')
-        return float(rewards[name_match]['TOWN Value'].iloc[0])
+        r = rewards[name_match]
+        n = len(r)
+        if n > 1:
+            print(f'Warning: {name} has {n} reward matches: {r}')
+        return float(r.reward.iloc[0])
     
     prices['Reward'] = prices.Name.map(get_reward)
     prices['DTC'] = prices['OS USD'] / (prices.Reward * coin_prices['town-star'])
     prices['LastUpdate'] = datetime.now().isoformat()
     prices.to_csv(prices_csv, index=False)
-    status_text.empty()
+    update_status('Done')
 
-col1, col2 = st.columns(2)
-with col1:
-    sort_option = st.selectbox(label='Sort By', options=['Change', 'DTC', 'Price', 'GS Qty'])
-with col2:
-    sort_order = st.selectbox(label='Order', options=['ASC', 'DESC'])
-prices = prices\
-    .sort_values(
-    {
-        'Change': 'OS Change',
-        'DTC': 'DTC',
-        'Price': 'OS USD',
-        'GS Qty': 'GS Qty'
-    }[sort_option], ascending=
-    {
-        'ASC': True,
-        'DESC': False
-    }[sort_order])
+item_count_text.write(f'Total: {len(prices)} items')
+
+with st.sidebar:    
+    arb_only = st.checkbox('Arb Only', value=False)
+    max_eth = st.slider('Max ETH', min_value=0.1, max_value=10.0, value=1.0, step=0.01)
+    dtc_warn_threshold = st.slider('DTC Alert Threshold', step=1, min_value=1, max_value=200, value=130)
 
 # Generate markdown table
 md_exclude_headers =  ['LastUpdate', 'OS Link', 'OS Qty', 'GS Link', 'GS Qty']
@@ -123,7 +164,7 @@ def generate_md_row(row):
             'Reward': lambda v: f'{v:.0f} (${v * coin_prices["town-star"]:.1f})',
             'DTC': lambda v: f'{v:.1f}',
             'Arb': lambda v: f'${v:,.0f}',
-            'Name': lambda v: f'{v} ([OS:{os_qty}]({row["OS Link"]}), [GS:{gs_qty}]({row["GS Link"]}))'
+            'Name': lambda v: f'{v}<br/>([OS:{os_qty}]({row["OS Link"]}), [GS:{gs_qty}]({row["GS Link"]}))'
         }.get(idx, lambda v: v)(val)))
     return ('|' + '|'.join(str_vals) + '|')
 
@@ -135,15 +176,28 @@ def generate_md_header(cols):
         ('|' + '|'.join('---' for _ in cols) + '|')
     ]
 
-arb_only = st.checkbox('Arb Only', value=False)
+search_text = st.text_input(label='Search Item: ', value='')
+
+# Filter and sort prices
+last_update_time = prices.LastUpdate.iloc[0]
 if arb_only:
     prices = prices[prices['Arb'] > 0]
-max_eth = st.slider('Max ETH', min_value=0.1, max_value=10.0, value=1.0, step=0.01)
+
+prices = prices[prices['OS ETH'] <= max_eth]\
+    .sort_values(price_sort_map.get(sort_option, sort_option),
+    ascending=
+    {
+        'ASC': True,
+        'DESC': False
+    }[sort_order])
+
+if search_text:
+    prices = prices[lowertrim(prices['Name']).str.contains(lowertrim(search_text))]
+
 notif_text = st.empty()
-roi_warn_threshold = st.slider('DTC threshold', step=1, min_value=1, max_value=150, value=110)
-f'### Last Update: {datetime.fromisoformat(prices.LastUpdate.iloc[0]).strftime("%Y-%m-%d %H:%M")}'
-md = '\n'.join(generate_md_header(prices.columns) + prices[prices['OS ETH'] <= max_eth].apply(generate_md_row, axis=1).values.tolist())
-st.markdown(md)
+f'### Last Update: {datetime.fromisoformat(last_update_time).strftime("%Y-%m-%d %H:%M")}'
+md = '\n'.join(generate_md_header(prices.columns) + prices.apply(generate_md_row, axis=1).values.tolist())
+st.write(md, unsafe_allow_html=True)
 
 def get_countdown():
     return ((page_last_refresh_time + timedelta(seconds=page_refresh_interval)) - datetime.now()).total_seconds()
@@ -159,9 +213,9 @@ audio_html = f"""
 """
 audio_widget = st.empty()
 while get_countdown() <= page_refresh_interval:
-    if len(prices[prices.DTC < roi_warn_threshold]) > 0:
-        notif_text.write(f'Check DTC < {roi_warn_threshold} !!! Refresh in ~{round(get_countdown())}s')
-        audio_widget.markdown(audio_html, unsafe_allow_html=True)
+    if len(prices[prices.DTC < dtc_warn_threshold]) > 0:
+        notif_text.write(f'Check DTC < {dtc_warn_threshold} !!! Refresh in ~{round(get_countdown())}s')
+        audio_widget.write(audio_html, unsafe_allow_html=True)
     else:
         notif_text.write(f'Will refresh in ~{round(get_countdown())}s')
         audio_widget.empty()
--- app/util.py
+++ app/util.py
@@ -4,6 +4,8 @@ import json
 import numpy as np
 import pandas as pd
 from multiprocessing.pool import ThreadPool
+from http.cookies import SimpleCookie
+import re
 
 from time import sleep
 
@@ -15,7 +17,54 @@ nan = float('nan')
 opensea_commission = 0.025
 opensea_townstar_commission = 0.025
 
-def fetch_gala_store_price(sell_order, symbol_preference=['TOWN', 'GALA', 'ETH', 'BAT']):
+def lowertrim(s):
+    if isinstance(s, pd.Series):
+        return s.str.lower().str.strip()
+    else:
+        return s.lower().strip()
+
+def initialize_nftlookup_io():
+    r = requests.get('https://nftlookup.io/nftlookup/NFT_index.cfm')
+    # Get CFID and CFTOKEN
+    cookiestr = re.sub(
+        'HttpOnly(, )?', '',
+        r.headers['Set-Cookie'])
+    cookie = SimpleCookie()
+    cookie.load(cookiestr)
+    # Get curToken
+    html = r.content.decode()
+    cur_token = re.search('curToken: \'(.+)\'', html).group(1)
+    return dict(curToken=cur_token, **{
+        k: v.value for k, v in cookie.items()
+    })
+
+nftlookup_io = initialize_nftlookup_io()
+
+def fetch_rewards():
+    h = {
+        'authority': 'nftlookup.io',
+        'pragma': 'no-cache',
+        'cache-control': 'no-cache',
+        'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'accept': 'application/json, text/javascript, */*; q=0.01',
+        'x-requested-with': 'XMLHttpRequest',
+        'origin': 'https://nftlookup.io',
+        'referer': 'https://nftlookup.io/nftlookup/NFT_index.cfm',
+        'cookie': f'CFID={nftlookup_io["CFID"]}; CFTOKEN={nftlookup_io["CFTOKEN"]}',
+    }
+    data = ('method=getTempTableInfo&curJson=%7B%22BasedOn%22%3A%22Items%22%2C%22DashBoardItemName%22%3A%22NFT+Rewards+for+Townstar+(Vox+%26+Townstar+NFT)%22%2C%22DashBoardItemDescription%22%3A%22This+dashboard+will+give+you+all+NFT\'s+that+are+used+in+Townstar%2C+ordered+by+their+rarity.+It+includes+as+well+the+current+price+on+the+market%22%2C%22DashboardID%22%3A%22-1%22%2C%22IsItemActivated%22%3A%221%22%2C%22Collections%22%3A%5B%22collectvox%22%2C%22town-star%22%5D%2C%22CollectionItems%22%3A%5B%5D%2C%22SelectFields%22%3A%5B%22itemname%22%2C%22colname%22%2C%22itemrarityscore%22%2C%22reppricelowETH%22%5D%2C%22Statements%22%3A%5B%5D%2C%22Footer%22%3A%5B%5D%2C%22FinalStatements%22%3A%5B%22itemname%22%2C%22colname%22%2C%22itemrarityscore%22%2C%22reppricelowETH%22%5D%2C%22OrderBy%22%3A%5B%22itemrarityscoreDesc%22%5D%2C%22LimitQuery%22%3A%5B%7B%22AndOr%22%3A%22And%22%2C%22FieldName%22%3A%22itemrarityscore%22%2C%22Type%22%3A%22Number%22%2C%22Operator%22%3A%22%3E%22%2C%22Value%22%3A%220%22%7D%5D%2C%22isGraph%22%3A%220%22%2C%22GraphType%22%3A%22%22%2C%22GraphTitle%22%3A%22%22%2C%22GraphLabel%22%3A%22%22%2C%22GraphValues%22%3A%22%22%2C%22isShowDetailsButton%22%3A%221%22%2C%22Alert%22%3A%5B%5D%7D&'
+        f'curToken={nftlookup_io["curToken"]}')
+    r = requests.post(url='https://nftlookup.io/nftlookup/GeneralComponents/DatabaseFunctions.cfc',
+        headers=h, data=data)
+    r_data = r.json()
+    rewards_df = pd.DataFrame(r_data['TableData'], columns=[v['title'] for v in r_data['TableColumns']])\
+        .rename(columns={
+            'Item Name': 'name',
+            'Rarity (Rewards)': 'reward'
+        })
+    return rewards_df
+
+def fetch_gala_store_price(sell_order, name, symbol_preference=['TOWN', 'GALA', 'ETH', 'BAT']):
     h = {
         "content-type": "application/json",
         "pragma": "no-cache",
@@ -70,11 +119,11 @@ def fetch_gala_store_price(sell_order, symbol_preference=['TOWN', 'GALA', 'ETH',
                         effective_symbol = pref_sym
                         break
             if usd_price is nan:
-                print(f'No {symbol_preference} price available in Gala Store for {gala_item_id}: only {symbols}')
+                print(f'No {symbol_preference} price available in Gala Store for {name}: only {symbols}')
         except IndexError:
-            print(f'No {symbol_preference} price available in Gala Store for {gala_item_id}')
+            print(f'No price available in Gala Store for {name}')
     else:
-        print(f'Failed to fetch Gala Store price for {gala_item_id}: {r.reason}')
+        print(f'Failed to fetch Gala Store price for {name}: {r.reason}')
     gs_link = f'https://app.gala.games/games/buy-item/0x{gala_item_id}/?currency={effective_symbol}'
     return gs_link, usd_price, qty
 
@@ -112,7 +161,7 @@ def fetch_gala_store_txn_fee(in_usd=True, symbol='TOWN'):
         data = r.json()['data']['transactionFeeEstimate']
         fee = float(data['gasUnitsEstimate']) * float(data['gasPriceEstimate']['low']) / 1e18
         if in_usd:
-            fee *= get_coin_price('ethereum')['ethereum']
+            fee *= fetch_coin_prices('ethereum')['ethereum']
     else:
         print('Failed to get Gala Store transaction fee')
     return fee
@@ -149,7 +198,7 @@ def fetch_gala_mint_fee(in_usd=True):
         txn_fee = fetch_gala_store_txn_fee(in_usd=False, symbol='ITEM')
         fee = send_amount + txn_fee
         if in_usd:
-            fee *= get_coin_price('ethereum')['ethereum']
+            fee *= fetch_coin_prices('ethereum')['ethereum']
     else:
         print('Failed to get Gala Store miting fee')
     return fee
@@ -177,32 +226,38 @@ def parse_prices(assets, n_workers=16):
 
     def thread_work(a):
         sell_orders = a['sell_orders']
-        if not sell_orders:
-            print(a['name'] + ' has no sell orders')
-            return None
-        cheapest_so_idx = np.argmin([parse_sell_order_price(so, symbol='usd') for so in sell_orders])
-        cheapest_so = sell_orders[cheapest_so_idx]
-        cheapest_price_eth = parse_sell_order_price(cheapest_so, symbol='eth')
-        cheapest_price_usd = parse_sell_order_price(cheapest_so, symbol='usd')
-        cheapest_so_qty = int(cheapest_so['quantity'])
-        last_sale = a['last_sale']
-        # last_sale_eth = parse_last_sale_price(last_sale, symbol='eth')
-        last_sale_usd = parse_last_sale_price(last_sale, symbol='usd')
-        # last_sale_qty = float(last_sale['quantity'])
-        print(f'Fetching price for {a["name"]} ({a["permalink"]})')
-        gs_link, gs_usd, gs_qty = fetch_gala_store_price(cheapest_so)
+        os_price_eth = nan
+        os_price_usd = nan
+        os_qty = nan
+        os_last_sale_usd = nan
+        gs_link = 'N/A'
+        gs_usd = nan
+        gs_qty = nan
+        if sell_orders:
+            cheapest_so_idx = np.argmin([parse_sell_order_price(so, symbol='usd') for so in sell_orders])
+            cheapest_so = sell_orders[cheapest_so_idx]
+            os_price_eth = parse_sell_order_price(cheapest_so, symbol='eth')
+            os_price_usd = parse_sell_order_price(cheapest_so, symbol='usd')
+            os_qty = int(cheapest_so['quantity'])
+            last_sale = a['last_sale']
+            # last_sale_eth = parse_last_sale_price(last_sale, symbol='eth')
+            os_last_sale_usd = parse_last_sale_price(last_sale, symbol='usd')
+            # last_sale_qty = float(last_sale['quantity'])
+            gs_link, gs_usd, gs_qty = fetch_gala_store_price(cheapest_so, name=a['name'])
+        else:
+            print(f'Warning: {a["name"]} has no sell orders')
         return [
             a['name'],
             a['permalink'],
-            cheapest_price_eth,
-            cheapest_price_usd,
-            max(cheapest_so_qty, 0),
-            100 * (cheapest_price_usd - last_sale_usd) / last_sale_usd,
+            os_price_eth,
+            os_price_usd,
+            max(os_qty, 0),
+            100 * (os_price_usd - os_last_sale_usd) / os_last_sale_usd,
             gs_link,
             gs_usd,
             max(gs_qty, 0),
-            max(0, cheapest_price_usd * (1 - opensea_commission - opensea_townstar_commission) -
-                (gs_usd + gs_fee + gs_mint_fee)) if gs_qty > 0 else nan
+            os_price_usd * (1 - opensea_commission - opensea_townstar_commission) -
+                (gs_usd + gs_fee + gs_mint_fee)
         ]
     with ThreadPool(n_workers) as pool:
         data = [i for i in pool.map(thread_work, assets) if i is not None]
@@ -212,19 +267,15 @@ def parse_prices(assets, n_workers=16):
         'GS Link', 'GS USD', 'GS Qty', 'Arb'])
 
 
-def get_coin_price(*coins):
+def fetch_coin_prices(*coins):
     coins_str = ','.join(coins)
     r = requests.get(url=f'https://api.coingecko.com/api/v3/simple/price?ids={coins_str}&vs_currencies=usd')
     assert r.status_code == 200
     return {k: v['usd'] for k, v in r.json().items()}
 
 
-def fetch_rewards(url=r'https://docs.google.com/spreadsheets/d/1z_18FrAbA9gMbyGn91j_Uf31lQW0ksUUHpPuGrXNHQY/export?format=csv&id=1z_18FrAbA9gMbyGn91j_Uf31lQW0ksUUHpPuGrXNHQY&gid=0'):
-    return pd.read_csv(url)
-
-
 def fetch_items(
-        st_empty=None,
+        update_status_callback=None,
         collection='town-star',
         contract_address='0xc36cf0cfcb5d905b8b513860db0cfe63f6cf9f5c',
         attribute_filter=None,
@@ -253,8 +304,8 @@ def fetch_items(
             limit=limit,
             offset=offset
         ))
-        if st_empty:
-            st_empty.write(f'Fetching offset={offset}')
+        if callable(update_status_callback):
+            update_status_callback(f'Fetching offset={offset}')
         ti_param_list = ['']
         if len(token_ids) > 0:
             ti_param_list = [
@@ -279,7 +330,7 @@ def fetch_items(
             if len(assets) == 0:
                 break
             for a in assets:
-                if attribute_filter is not None and not attribute_filter(a):
+                if callable(attribute_filter) and not attribute_filter(a):
                     continue
                 if has_desired_trait(a['traits']):
                     yield a
